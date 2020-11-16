fbcada6a7ecdf820a58d9865952d8c244fd13d69
JMJ Riyana <jeemariyana@gmail.com>
2020-11-16 19:09:19 +0530

    book

--- src/main/java/com/example/model/Book.java
+++ src/main/java/com/example/model/Book.java
@@ -3,6 +3,7 @@ package com.example.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
@@ -10,6 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @ToString
 @Document(collection = "Book")
 public class Book {
+    @Id
     private int id;
     private String bookName;
     private  String authorName;
---
+++ src/main/java/com/example/repository/BookRepository.java
@@ -0,0 +1,6 @@
+package com.example.repository;
+import com.example.model.Book;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface BookRepository extends MongoRepository<Book, Integer> {
+}
